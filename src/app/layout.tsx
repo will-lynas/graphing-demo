9fe165e2b9ed83eb42bed2497ab3de73c6b32dfd
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-04-20 22:45:23 +0100

    Make it dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
         {children}
       </body>
