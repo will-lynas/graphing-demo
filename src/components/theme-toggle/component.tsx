27db7b498eae33ee7f2ddc912844c1c73bbe6218
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-04-21 17:59:09 +0100

    Make comment better

--- src/components/theme-toggle/component.tsx
+++ src/components/theme-toggle/component.tsx
@@ -8,7 +8,10 @@ export default function ThemeToggleComponent() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  // prevent hydration error
+  // This is to prevent hydration error
+  // The server does not have access to the theme (stored in localStorage)
+  // In order to make the client render the same as the server, we initially set the theme to undefined
+  // Once the component is mounted, we set the theme to the correct value
   useEffect(() => {
     setMounted(true);
   }, []);
