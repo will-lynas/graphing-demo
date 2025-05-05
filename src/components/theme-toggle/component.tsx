e7ceb814e516000fe50b949a53628035d7daf417
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-05-05 15:49:40 +0100

    Simplify the hydration error fix

--- src/components/theme-toggle/component.tsx
+++ src/components/theme-toggle/component.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useTheme } from "next-themes";
-import ThemeToggle from "./inner";
+import { Monitor, Moon, Sun } from "lucide-react";
+import ThemeToggleButton from "./button";
 import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
-export default function ThemeToggleComponent() {
-  const { theme, setTheme } = useTheme();
+export default function ThemeToggle() {
   const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   // This is to prevent hydration error
   // The server does not have access to the theme (stored in localStorage)
@@ -16,9 +17,33 @@ export default function ThemeToggleComponent() {
     setMounted(true);
   }, []);
 
-  if (!mounted) {
-    return <ThemeToggle theme={undefined} setTheme={setTheme} />;
-  }
+  const safeTheme = mounted ? theme : undefined;
 
-  return <ThemeToggle theme={theme} setTheme={setTheme} />;
+  return (
+    <div className="flex items-center justify-end gap-4 p-4">
+      <div className="inline-flex items-center rounded-lg border p-1 shadow-sm bg-card gap-0.5">
+        <ThemeToggleButton
+          thisTheme="light"
+          theme={safeTheme}
+          setTheme={setTheme}
+        >
+          <Sun className="h-5 w-5" />
+        </ThemeToggleButton>
+        <ThemeToggleButton
+          thisTheme="dark"
+          theme={safeTheme}
+          setTheme={setTheme}
+        >
+          <Moon className="h-5 w-5" />
+        </ThemeToggleButton>
+        <ThemeToggleButton
+          thisTheme="system"
+          theme={safeTheme}
+          setTheme={setTheme}
+        >
+          <Monitor className="h-5 w-5" />
+        </ThemeToggleButton>
+      </div>
+    </div>
+  );
 }
